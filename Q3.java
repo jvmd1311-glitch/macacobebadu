33fd33323d6a608e9b2807d95889aa11cdf91349
Dromedario <jvmd1311@gmail.com>
2026-08-26 16:52:36 -0300

    Add files via upload

--- Q3.java
+++ Q3.java
@@ -1,26 +1,58 @@
-import java.util .*;
-class Q3{
-	public static boolean verificarletras(String string, int tam){
-		for(int i=0 ; i<tam ; i++){
-		 if((string.charAt(i) >='A'&& string.charAt(i)<= 'Z') ||string.charAt(i) >='a'&& string.charAt(i)<= 'z'){
-			 return true;
-		 }
-		}
-	public static boolean vogal(String string, int tam){
-	 for(int i=0 ; i<tam ; i++){	       
-		if(!(string.charAt(i) == 'a' ||string.charAt(i) == 'e' ||string.charAt(i) == 'i' ||string.charAt(i) == 'o' ||string.charAt(i) == 'u' string.charAt(i) == 'A' ||string.charAt(i) == 'E' ||string.charAt(i) == 'I' ||string.charAt(i) == 'O' ||string.charAt(i) == 'U'))
-return false;
-		 }
-else return true;
-}
+import java.util.*;
+
+class Q3 {
+
+    public static boolean ehLetra(char c) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+    }
+
+    public static boolean vogal(String string, int tam) {
+        for (int i = 0; i < tam; i++) {
+            char c = string.charAt(i);
+            if (!ehLetra(c) || !(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')) {
+
+                return false;
+            }
+        }
+	return true; 
+    }
+
+    public static boolean consoante(String string, int tam) {
+        for (int i = 0; i < tam; i++) {
+            char c = string.charAt(i);
+            boolean ehVogal = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+            
+            if (!ehLetra(c) || ehVogal) {
+                return false;
+            }
+        }
+	return true; 
+    }
+      public static boolean inteiro(String string, int tam) {
+	for(int i=0; i<tam ; i++){
+		char c = string.charAt(i);
+		if(c >=0 && c <=9){
+			return true ;
 	}
-        public static boolean consoante(String string, int tam){
-            for(int i=0 ; i<tam ; i++){
-                   !vogal(string, tam);
-  } 
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String entrada = sc.nextLine();
+        int tam = entrada.length();
+	if(vogal(entrada,tam)){
+	System.out.print("SIM ");
+	}
+	if(!vogal(entrada,tam)){
+		System.out.print("NAO ");
+	}
+	if(consoante(entrada,tam)){
+          System.out.print("SIM ");
+          }
+          if(!consoante(entrada,tam)){
+                 System.out.print("NAO ");
+          }
+
+       	}
 }
-
-
-     public static void main(String[] args){
-	    Scanner sc = new Scanner(System.in); 
-     }
