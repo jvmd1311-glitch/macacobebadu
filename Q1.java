ebe0f60d7fb7dbd4b6cd59ef547bc039f74f77ca
Dromedario <jvmd1311@gmail.com>
2026-08-26 14:28:28 -0300

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str1 = sc.nextLine();
+        
+        // Testa se a linha lida é "FIM"
+        while (!str1.equals("FIM")) {
+            String fstr = "";
+            int n = str1.length(); // Pega o tamanho da String
+            
+            for (int i = 0; i < n; i++) {
+                fstr += (char) (str1.charAt(i) + 3); // Cifra de César
+            }
+            
+            System.out.println(fstr);
+            
+            // Lê a próxima linha para a próxima iteração
+            str1 = sc.nextLine();
+        }
+    }
+}
+
