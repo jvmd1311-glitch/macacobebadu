5f33f0b40a7e0c62a3bfdb085dedbff84ebc5289
Dromedario <jvmd1311@gmail.com>
2026-08-28 15:10:17 -0300

    Add files via upload

--- Q9.c
+++ Q9.c
@@ -1,19 +1,30 @@
-#include #stdio.h>
-int cesarRecursivo(char str[], int i){
-        if(str[i] == '/0'){
-            return str;
-        }
-        str[i] = str[i] + 3;
-        cesarRecursivo(str, i + 1);
+#include <stdio.h>
+
+void cesarRecursivo(char str[], int i){
+    if(str[i] == '\0'){
+        return;
     }
 
+    // Troca o caractere atual pelo caractere 3 posições a frente
+    str[i] = str[i] + 3;
+
+    // Vai para o proximo caractere e repete o processo
+    cesarRecursivo(str, i + 1);
+}
+
+// Começa a recursao no primeiro caractere da string
+void cesar(char str[]){
+        cesarRecursivo(str,0);
+}
 
 int main(){
     char str[100];
+    int i;
     fgets(str, 100, stdin);
-    while (!(str[0] == 'F' && str[1] == 'I' && str[2] == 'M' && str[3] == '\0')) {
-        cesarRecursivo(str, 0);
+    // Continua lendo ate a entrada ser FIM
+    while (!(str[0] == 'F' && str[1] == 'I' && str[2] == 'M' && str[3] == '\n')) {
+        cesar(str);
         printf("%s\n", str);
         fgets(str, 100, stdin);
+        }
     }
-}
--- q5.c
+++ q5.c
@@ -1,16 +1,21 @@
 #include  <stdio.h>
 
 int soma(int num){
-        int num;
         int soma=0;
+
+        // Vai pegando e somando um algarismo por vez
         while(num!=0){
+            // Pega o último algarismo do número
             soma += num % 10;
+
+            // Remove o último algarismo para continuar
            num /= 10 ;
         }
     return soma;
 }
 int main(){
         int v;
+
          while (scanf("%d", &v) != EOF) {
         printf("%d\n", soma(v));
     }
