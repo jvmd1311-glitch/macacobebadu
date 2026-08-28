6ee738eca6c51b864212a89f870fed091a797c38
Dromedario <jvmd1311@gmail.com>
2026-08-28 15:22:22 -0300

    Add files via upload

--- Q9.c
+++ Q9.c
@@ -3,13 +3,14 @@
 void cesarRecursivo(char str[], int i){
     if(str[i] == '\0'){
         return;
-    }
+    } else {
 
     // Troca o caractere atual pelo caractere 3 posições a frente
     str[i] = str[i] + 3;
 
     // Vai para o proximo caractere e repete o processo
     cesarRecursivo(str, i + 1);
+}
 }
 
 // Começa a recursao no primeiro caractere da string
@@ -20,11 +21,22 @@ void cesar(char str[]){
 int main(){
     char str[100];
     int i;
-    fgets(str, 100, stdin);
-    // Continua lendo ate a entrada ser FIM
-    while (!(str[0] == 'F' && str[1] == 'I' && str[2] == 'M' && str[3] == '\n')) {
+        fgets(str, 100, stdin);
+        while (1) {
+            for (i = 0; str[i] != '\0'; i++) {
+                if (str[i] == '\n') {
+                    str[i] = '\0';
+                    break;
+                }
+            }
+
+            // Para quando a entrada for FIM.
+            if (str[0] == 'F' && str[1] == 'I' && str[2] == 'M' && str[3] == '\0') {
+                break;
+            }
         cesar(str);
         printf("%s\n", str);
         fgets(str, 100, stdin);
         }
+    }
     }
