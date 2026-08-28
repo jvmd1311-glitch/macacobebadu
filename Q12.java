4b0fa5b1c681fa59efe984766aabcc5d96a622df
Dromedario <jvmd1311@gmail.com>
2026-08-28 17:39:12 -0300

    Add files via upload

--- Q12.java
+++ Q12.java
@@ -1,11 +1,14 @@
 import java.util.*;
 
 class Q12{
+        // Soma recursivamente os algarismos do numero
         public static int somar(int num){
+                // Caso-base da recursao.
                 if(num/10==0){
                      return num;
                 }
-                
+
+                // Soma o ultimo algarismo aos demais
                 return num%10 + somar(num/10);
         }
 
@@ -13,6 +16,7 @@ class Q12{
                 Scanner sc=new Scanner(System.in);
                 int num;
                 num=sc.nextInt();
+                // Processa os numeros ate encontrar 250
                 while(num!=250){
                         System.out.println(somar(num));
                         num=sc.nextInt();
